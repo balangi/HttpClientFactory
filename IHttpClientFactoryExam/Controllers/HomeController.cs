aed3fe24b1bf53d43a1d688262047165a26c97ba
balangi <balangi@gmail.com>
2023-04-03 10:35:19 +0430

    Create Project

---
+++ IHttpClientFactoryExam/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IHttpClientFactoryExam.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public HomeController(
+            ILogger<HomeController> logger,
+            IHttpClientFactory clientFactory)
+        {
+            _logger = logger;
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var client = _clientFactory.CreateClient("Git");
+            var result = await client.GetStringAsync("/");
+
+            return Ok(result);
+        }
+    }
+}
